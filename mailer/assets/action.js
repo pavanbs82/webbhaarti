6ad01f7d48e50de74c6b455ebe94a58b7d0e4c92
pavanbs82 <stapler145@gmail.com>
2018-08-24 23:19:06 +0530

    signup page and lightbox UI

--- mailer/assets/action.js
+++ mailer/assets/action.js
@@ -1,3 +1,21 @@
 function showAddNewGroup() {
    document.getElementById('addNewGroup').style.display = "block";
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "318px";
+    document.getElementById("main").style.marginLeft = "318px";
+}
+function closeNav(){
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+function showLightBox() {
+   document.getElementById('lightBox').style.display = "flex";
+}
+
+function closeLightBox(){
+    document.getElementById("lightBoxClose").style.display = "none";
+    document.getElementById("lightBox").style.width = "0";
+}
